c0ec1b576e7538809054dbba2b902acda80bdd79
salsablsf <salsablsf@gmail.com>
2022-06-29 14:39:03 +0700

    Memberikan rating

--- HereForYou/resources/views/admin/pages/booking/index.blade.php
+++ HereForYou/resources/views/admin/pages/booking/index.blade.php
@@ -6,6 +6,7 @@
             <div class="card-header">
                 <div class="d-flex justify-content-between">
                     <h6 class="text-dark">Data Booking</h6>
+                    <a href="{{ route('admin.bookings.create') }}" class="btn btn-sm btn-primary">Buat Booking</a>
                 </div>
             </div>
             <div class="card-body">
@@ -47,7 +48,7 @@
                                     <span class="badge badge-success">SUCCESS</span>
                                     @elseif($item->status === 'PENDING')
                                     <span class="badge badge-info">PENDING</span>
-                                    @else 
+                                    @else
                                     <span class="badge badge-danger">FAILED</span>
                                     @endif
                                 </td>
@@ -63,6 +64,7 @@
                                     <a href="{{ route('admin.bookings.set-status',$item->id) }}?status=SUCCESS" class="btn btn-sm btn-success"><i class="fas fa-check"></i></a>
                                     <a href="{{ route('admin.bookings.set-status',$item->id) }}?status=PENDING" class="btn btn-sm btn-info"><i class="fas fa-spinner"></i></a>
                                     @endif
+                                    <a href="{{ route('admin.bookings.edit',$item->id) }}" class="btn btn-sm btn-info"><i class="fas fa-edit"></i></a>
                                     <form action="{{ route('admin.bookings.destroy', $item->id) }}" class="d-inline" method="post">
                                         @csrf
                                         @method('delete')
@@ -78,37 +80,5 @@
     </div>
 </div>
 @endsection
-@push('styles')
-<!-- DataTables -->
-<link rel="stylesheet" href="{{ asset('assets/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
-<link rel="stylesheet" href="{{ asset('assets/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
-<!-- Toastr -->
-<link rel="stylesheet" href="{{ asset('assets/plugins/toastr/toastr.min.css') }}">
-@endpush
-@push('scripts')
-<!-- DataTables  & Plugins -->
-<script src="{{ asset('assets/plugins/datatables/jquery.dataTables.min.js') }}"></script>
-<script src="{{ asset('assets/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
-<script src="{{ asset('assets/plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
-<script src="{{ asset('assets/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
-<!-- Toastr -->
-<script src="{{ asset('assets/plugins/toastr/toastr.min.js') }}"></script>
-
-<!-- Page specific script -->
-<script>
-  $(function () {
-    $('#table').DataTable({
-        "paging": true,
-        "ordering": true,
-        "info": true,
-        "autoWidth": true,
-        "responsive": true,
-    });
-  });
-</script>
-@if (session('success'))
-<script>
-    toastr.success('{{ session('success') }}.')
-</script>
 @endif
 @endpush
