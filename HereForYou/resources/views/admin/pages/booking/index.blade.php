fc7153952e2073e04cc9f4bfae378b1b5c4f8345
yolandahafitzhah <nandatata57@gmail.com>
2022-06-29 14:43:28 +0700

    LogOut

--- HereForYou/resources/views/admin/pages/booking/index.blade.php
+++ HereForYou/resources/views/admin/pages/booking/index.blade.php
@@ -80,5 +80,37 @@
     </div>
 </div>
 @endsection
+@push('styles')
+<!-- DataTables -->
+<link rel="stylesheet" href="{{ asset('assets/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
+<!-- Toastr -->
+<link rel="stylesheet" href="{{ asset('assets/plugins/toastr/toastr.min.css') }}">
+@endpush
+@push('scripts')
+<!-- DataTables  & Plugins -->
+<script src="{{ asset('assets/plugins/datatables/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
+<!-- Toastr -->
+<script src="{{ asset('assets/plugins/toastr/toastr.min.js') }}"></script>
+
+<!-- Page specific script -->
+<script>
+  $(function () {
+    $('#table').DataTable({
+        "paging": true,
+        "ordering": true,
+        "info": true,
+        "autoWidth": true,
+        "responsive": true,
+    });
+  });
+</script>
+@if (session('success'))
+<script>
+    toastr.success('{{ session('success') }}.')
+</script>
 @endif
 @endpush
